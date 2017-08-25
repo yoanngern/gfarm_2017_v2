983ab57fcb67c0f369047932a8c31df6bc107406
Yoann Gern <yoann@yoanngern.ch>
2017-08-25 14:12:27 +0200

    update events

--- wp-content/themes/gfarm_2017_v2/template-parts/event/list.php
+++ wp-content/themes/gfarm_2017_v2/template-parts/event/list.php
@@ -15,6 +15,8 @@
 
 		<?php
 
+		query_posts( 'gfarm_eventcategory=evenement' );
+
 
 		if ( have_posts() ) : ?>
 
@@ -50,6 +52,55 @@
 
 		endif;
 		?>
+
+
+        <header>
+
+            <h1><?php pll_e( 'Brunch' ); ?></h1>
+
+        </header>
+
+
+	    <?php
+
+	    query_posts( 'gfarm_eventcategory=brunch' );
+
+
+	    if ( have_posts() ) : ?>
+
+
+            <section id="listOfEvents" class="small" data-nb="3">
+                <article class="content-page">
+
+
+				    <?php
+
+				    /* Start the Loop */
+				    while ( have_posts() ) :
+					    the_post();
+
+					    get_template_part( 'template-parts/event/item' );
+
+				    endwhile; ?>
+
+                </article>
+            </section>
+
+
+            <nav class="nav_bottom">
+                <div class="nav-previous alignleft"><?php previous_posts_link( 'Previous' ); ?></div>
+                <div class="nav-next alignright"><?php next_posts_link( 'Next' ); ?></div>
+            </nav>
+
+		    <?php
+
+	    else :
+
+		    get_template_part( 'template-parts/event/none' );
+
+	    endif;
+	    ?>
+
     </div>
 
 </section>
